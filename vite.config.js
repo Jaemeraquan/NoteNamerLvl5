755704f5f623c57484b20c7eb716051508c8694d
Jaemeraquan <138228919+Jaemeraquan@users.noreply.github.com>
2024-01-10 10:09:54 -0500

    npm install

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NoteNamerLvl5"
+  base: "/NoteNamerLvl5/"
   
 })
